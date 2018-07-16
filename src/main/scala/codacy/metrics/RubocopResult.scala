26562b7e1b2a4b6b8ad7804c64d39f89f494a7f7
Hugo Sousa <hugo@codacy.com>
2018-07-16 16:38:45 +0100

    Remove unnecessary json fields parsing

--- src/main/scala/codacy/metrics/RubocopResult.scala
+++ src/main/scala/codacy/metrics/RubocopResult.scala
@@ -2,35 +2,18 @@ package codacy.metrics
 
 import play.api.libs.json.{Format, JsString, JsValue, Json}
 
-//TODO: do we really need all of this? can't we just parse the message we need from json to get the complexity?
-
 case class RubocopLocation(line: Int, column: JsValue, length: JsValue)
 
-case class RubocopMetadata(rubocop_version: JsValue,
-                           ruby_engine: JsValue,
-                           ruby_version: JsValue,
-                           ruby_patchlevel: JsValue,
-                           ruby_platform: JsValue)
-
-case class RubocopOffense(severity: JsValue,
-                          message: JsString,
-                          cop_name: JsString,
-                          corrected: JsValue,
+case class RubocopOffense(message: JsString,
                           location: RubocopLocation)
 
 case class RubocopFile(path: JsString, offenses: Option[List[RubocopOffense]])
 
-case class RubocopSummary(offense_count: JsValue, target_file_count: JsValue, inspected_file_count: JsValue)
-
-case class RubocopResult(metadata: RubocopMetadata, files: Option[List[RubocopFile]], summary: RubocopSummary)
+case class RubocopResult(files: Option[List[RubocopFile]])
 
 object RubocopResult {
   implicit val RLocation: Format[RubocopLocation] = {
     Json.format[RubocopLocation]
-  }
-
-  implicit val RMetaData: Format[RubocopMetadata] = {
-    Json.format[RubocopMetadata]
   }
 
   implicit val ROffenses: Format[RubocopOffense] = {
@@ -39,10 +22,6 @@ object RubocopResult {
 
   implicit val RFiles: Format[RubocopFile] = {
     Json.format[RubocopFile]
-  }
-
-  implicit val RSummary: Format[RubocopSummary] = {
-    Json.format[RubocopSummary]
   }
 
   implicit val RResult: Format[RubocopResult] = {
